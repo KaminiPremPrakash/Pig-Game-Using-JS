48c5065e4868222e5cc9a70c4805755c057e1f7a
Kamini Prakash <46829105+KaminiPremPrakash@users.noreply.github.com>
2020-09-04 22:08:43 -0400

    modifications

--- piggame.js
+++ piggame.js
@@ -1,4 +1,4 @@
-var scores, roundScore, activeplayer;
+var scores, roundScore, activeplayer, gamePlaying;
 
 reset(); //to initialize variables
 //dice = Math.floor(Math.random() * 6) + 1; // to generate numbs from 1 to 6 randomly
@@ -6,29 +6,33 @@ reset(); //to initialize variables
 //var x = document.querySelector('#score-0').textContent;
 //console.log(x);
 //to hide the dice
-document.querySelector('.dice').style.display = "none";
+//document.querySelector('.dice').style.display = "none";
 //we dont write btn() bcz we dont want to call it, it will be called by event listener
 
 //document.querySelector('.btn-roll').addEventListener('click', btn);
 
 //we can  write an anonymous fcn istead of btn// anonymous fcn cant be used again
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    var dice = Math.floor(Math.random() * 6) + 1; //create a random number between 1-6
-    document.querySelector('.dice').style.display = "block";
-    document.querySelector('.dice').src = 'dice' + dice + '.png';
+document.querySelector('.btn-roll').addEventListener('click', function () {
 
-    if (dice > 1) {
-        //add score
-        roundScore = roundScore + dice;
-        document.querySelector('#current-' + activeplayer).textContent = roundScore;
-    } else {
-        //goes to next player
-        switchPlayer();
+    if (gamePlaying) {
+
+        var dice = Math.floor(Math.random() * 6) + 1; //create a random number between 1-6
+        var diceDOM = document.querySelector('.dice')
+        diceDOM.querySelector('.dice').style.display = "block";
+        diceDOM.querySelector('.dice').src = 'dice' + dice + '.png';
+
+        if (dice !== 1) {
+            //add score
+            roundScore = roundScore + dice;
+            document.querySelector('#current-' + activeplayer).textContent = roundScore;
+        } else {
+            //goes to next player
+            switchPlayer();
+        }
     }
-
 });
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
+document.querySelector('.btn-hold').addEventListener('click', function () {
     //add current score to player's global score
     scores[activeplayer] += roundScore;
     //update the user interface
@@ -39,20 +43,23 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#name-' + activeplayer).style.color = "Red";
         document.querySelector('#name-' + activeplayer).style.fontweight = "Bold";
         document.querySelector('.dice').style.display = 'none';
+        gamePlaying = false;
     }
-    switchPlayer();
+    else {
+        switchPlayer();
+    }
 
 });
 
 
 
 function switchPlayer() {
-    roundScore = 0;
     if (activeplayer === 0) {
         activeplayer = 1;
     } else {
         activeplayer = 0;
     }
+    roundScore = 0;
     document.getElementById('current-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
 
@@ -69,11 +76,14 @@ function reset() {
     score = [0, 0];
     activeplayer = 0;
     roundScore = 0;
+    gamePlaying = true;
     document.querySelector('.dice').style.display = "none";
     //setting scores to 0
-    document.getElementById('score-0').textContent = '0';
-    document.getElementById('score-1').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.getElementById('current-0').textContent = '0';
-    document.querySelector('#name-' + activeplayer).textContent = 'PLAYER ' + activeplayer;
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 }
